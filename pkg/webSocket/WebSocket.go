5ab1ac84f5f3c8b6d6ef9902790508075497a76a
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-03-20 15:14:48 +0100

    add(test): Adding new testcases and some comments

--- pkg/webSocket/WebSocket.go
+++ pkg/webSocket/WebSocket.go
@@ -6,6 +6,9 @@ import (
 	"sync"
 )
 
+/*
+WebSocket type definition.
+*/
 type WebSocket struct {
 	Conn  *websocket.Conn
 	mutex sync.Mutex
@@ -16,12 +19,18 @@ type WebSocket struct {
 	limiter   *rate.Limiter
 }
 
+/*
+WriteJSON writes a output JSON to the websocket with mutual exclusion activated.
+*/
 func (ws *WebSocket) WriteJSON(any interface{}) error {
 	ws.mutex.Lock()
 	defer ws.mutex.Unlock()
 	return ws.Conn.WriteJSON(any)
 }
 
+/*
+WriteMessage writes a output to the websocket with mutual exclusion activated.
+*/
 func (ws *WebSocket) WriteMessage(t int, b []byte) error {
 	ws.mutex.Lock()
 	defer ws.mutex.Unlock()
